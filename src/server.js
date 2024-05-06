70fd14a1007ba4f84e319a9e8ffd7cbc3bba5874
Gaspari <lcsgaspari@gmail.com>
2024-05-06 20:44:41 -0300

    initial project structure

---
+++ src/server.js
@@ -0,0 +1,23 @@
+//A boa prática é sempre chamar as dependencias que foram instaladas, depois as qual você criou no projeto.
+//dependencias instaladas
+const express = require('express');
+//const path = require('path');
+
+//dependencias criadas
+//const db = require('./database');
+const routes = require('./routes/routes');
+
+const app = express();
+
+//conexão com o banco de dados
+//db.connect()
+
+//Habilitada o server para receber dados via post (formulário)
+app.use(express.urlencoded({ extended: true }))
+
+//Definindo as rotas
+app.use('/api', routes)
+
+//Executando o servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
