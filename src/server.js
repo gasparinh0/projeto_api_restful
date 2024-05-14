e87aaa3802b38d8d3370d8c7f85adeb5d5dce649
Gaspari <lcsgaspari@gmail.com>
2024-05-13 21:00:27 -0300

    adding CORS middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 //A boa prática é sempre chamar as dependencias que foram instaladas, depois as qual você criou no projeto.
 //dependencias instaladas
 const express = require('express');
-//const path = require('path');
+const cors = require('cors');
 
 //dependencias criadas
 const db = require('./database/db');
@@ -11,6 +11,22 @@ const app = express();
 
 //conexão com o banco de dados
 db.connect()
+
+const allowedOrigins = [
+   'http://127.0.0.1:5500',
+   'http://www.app.com.br',
+]
+
+//habilitando o cors
+app.use(cors({
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+    }
+}))
 
 //habilita server para receber dados em formato json
 app.use(express.json())
